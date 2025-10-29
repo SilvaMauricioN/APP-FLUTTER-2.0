8bfe72df7ef141943e2868e4df91cb28df738d91
Silva Mauricio Nicolás <113395913+SilvaMauricioN@users.noreply.github.com>
2025-10-28 22:45:20 -0300

    Agregar nueva ruta api

--- lib/providers/museo_service.dart
+++ lib/providers/museo_service.dart
@@ -2,25 +2,36 @@ import 'package:http/http.dart' as http;
 import 'package:app_demo/screens/screens.dart';
 
 class MuseumService with ChangeNotifier {
-  
-  final String baseUrl = 'https://museorijks.onrender.com/api/MuseoRijks';  
+  final String baseUrl = 'http://192.168.0.83:8080/api/museorijks';
+  static const Map<String, String> defaultHeaders = {
+    'api-key': 'NjSSMo3S0L',
+    'Content-Type': 'application/json',
+  };
+
+  // final String baseUrl = 'https://museorijks.onrender.com/api/MuseoRijks';
 
   Future<List<Artistas>> getArtistas() async {
-    final response = await http.get(Uri.parse('$baseUrl/Artistas'));
+    final response = await http.get(
+        Uri.parse(
+          '$baseUrl/Artistas',
+        ),
+        headers: defaultHeaders);
 
     if (response.statusCode == 200) {
-      List<Artistas> listaArtistas = artistasFromJson(response.body);      
+      List<Artistas> listaArtistas = artistasFromJson(response.body);
       return listaArtistas;
     } else {
       throw Exception('Error al Cargar  los Artistas');
     }
-  }   
+  }
 
   Future<List<ObraSimple>> getObrasPorArtista(String nombreArtista) async {
-    final response = await http.get(Uri.parse('$baseUrl/Obras/Artista?nombreArtista=$nombreArtista'));
+    final response = await http.get(
+        Uri.parse('$baseUrl/collecion/Artista?artista=$nombreArtista'),
+        headers: defaultHeaders);
 
     if (response.statusCode == 200) {
-      List<ObraSimple> listaObrasArtista = obrasSimpleFromJson(response.body);      
+      List<ObraSimple> listaObrasArtista = obrasSimpleFromJson(response.body);
       return listaObrasArtista;
     } else {
       throw Exception('Error al Cargar las Obras del Artista');
@@ -28,10 +39,11 @@ class MuseumService with ChangeNotifier {
   }
 
   Future<ObraDetalle> getObraPorId(String id) async {
-     final response = await http.get(Uri.parse('$baseUrl/Obra/$id'));
+    final response =
+        await http.get(Uri.parse('$baseUrl/Obra/$id'), headers: defaultHeaders);
 
     if (response.statusCode == 200) {
-      ObraDetalle obraArtista = obraDetalleFromJson(response.body);      
+      ObraDetalle obraArtista = obraDetalleFromJson(response.body);
       return obraArtista;
     } else {
       throw Exception('Error al Cargar la Obra');
@@ -39,14 +51,14 @@ class MuseumService with ChangeNotifier {
   }
 
   Future<List<ObraSimple>> getColeccion() async {
-    final response = await http.get(Uri.parse('$baseUrl/Coleccion'));
+    final response = await http.get(Uri.parse('$baseUrl/obras/coleccion'),
+        headers: defaultHeaders);
 
     if (response.statusCode == 200) {
-      List<ObraSimple> listaColecionObras = obrasSimpleFromJson(response.body);      
+      List<ObraSimple> listaColecionObras = obrasSimpleFromJson(response.body);
       return listaColecionObras;
     } else {
       throw Exception('Error al Cargar la Coleccion');
     }
   }
-
 }
